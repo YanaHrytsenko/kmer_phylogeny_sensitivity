9a0d63aa10d0c8c4eeef5c72c4a5485bf93cfd8a
YanaHrytsenko <39499301+YanaHrytsenko@users.noreply.github.com>
2021-03-20 20:41:13 -0400

    added step 8 plot rf scores

---
+++ wgs_analysis/plot_RF_scores_step_8/batch_violin_plots.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#Yana Hrytsenko
+
+#module load matplotlib/3.1.1-foss-2019b-Python-3.7.4
+
+
+for k in  4 5 6 7 8 9 10 11 12 16
+do
+    rf_score_dirs="wgs_analysis/compare_phylogenies_step_7/CosDist_RF_scores/${k}Mers_RF_scores/"
+
+    python3 violin_plot_RF_scores.py ${k} ${rf_score_dirs} ${k}_Mers_RF_scores_violin_plot_sim_WGS_CosineDist.pdf
+
+done
---
+++ wgs_analysis/plot_RF_scores_step_8/calc_RF_scores_statistics.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python3
+#Yana Hrytsenko
+
+import sys
+from scipy import stats
+import numpy as np
+from statistics import mean, median, stdev
+
+
+kmer = sys.argv[1]
+path_to_score_files = sys.argv[2]
+stats_file_out = sys.argv[3]
+
+mut_rates = ['0.001', '0.01', '0.1', '1.0', '10.0']
+
+f = open(stats_file_out, "a")
+
+f.write('\n')
+
+header_line = 'Statistics for ' + kmer + '_Mers: ' + '\n'
+
+f.write(header_line)
+
+devider_line = '==========================' + '\n'
+
+f.write(devider_line)
+
+for i in range(len(mut_rates)):
+
+    rf_scores = np.genfromtxt(path_to_score_files + kmer + '_mers_' + mut_rates[i] + '_RF_Scores.txt', dtype=np.int32)
+
+    stdev_rf = stdev(float(item) for item in list(rf_scores))
+
+    mean_rf = float(mean(list(rf_scores)))
+
+    median_rf = float(median(list(rf_scores)))
+
+    stderr_rf = float(stats.sem(list(rf_scores)))
+
+    subheader_line1 = kmer + '_Mers ' + mut_rates[i] + ' mutation rate: '+ '\n'
+
+    subheader_line2 = 'mean     ' + 'median     ' + 'std_dev        ' + '   std_err    ' + '\n'
+
+    f.write(subheader_line1)
+
+    f.write(subheader_line2)
+
+    stats_line = str(mean_rf) + '       ' + str(median_rf) + '      ' + str(stdev_rf) + '   ' + str(stderr_rf) + '\n'
+
+    f.write(stats_line)
+
+    f.write('\n')
+
+
+f.close()
